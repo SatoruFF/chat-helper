38f49eb3b61a27270c9ae84b1fa45854e299c71e
Yagami Light <90285902+Yagamilighttt@users.noreply.github.com>
2023-05-16 23:32:24 +0300

    add some logic

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+import axios from "axios";
 
+const message = document.querySelector('.message');
+const sendButton = document.querySelector('.send-btn');
+const inputMessage = document.querySelector('.message-input');
+
+let questions = []
+let answer = []
+
+sendButton.addEventListener('click', () => {
+    if (inputMessage.value.length > 0) {
+        sendMessage(inputMessage.value)
+    }
+})
+
+function sendMessage(val) {
+    questions.push(val)
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('question');
+    newMessage.textContent = val
+    message.appendChild(newMessage)
+    inputMessage.value = ''
+}
+
+function getQuestion() {
+
+}
